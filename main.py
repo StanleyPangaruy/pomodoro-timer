abfba3abd545255e6e4ea676a2d9707662e4c379
unknown <pangaruystanley@gmail.com>
2022-11-19 13:56:16 +0800

    implemented tabs for pomodoro, short break and long break

--- main.py
+++ main.py
@@ -10,7 +10,34 @@ class pomodoroTimer:
         self.root.title("Pomodo Timer")
         self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="Tomato.png"))
 
+        self.s = ttk.Style()
+        self.s.configure("TNotebook.tab", font = ("Ubuntu", 16))
+        self.s.configure("TButton", font = ("Ubuntu", 16))
+
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill="both", pady=10, expand=True)
+
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Short Break")
+        self.tabs.add(self.tab3, text="Long Break")
+
         self.root.mainloop()
+
+    def startTimerThread(self):
+        pass
+
+    def startTimer(self):
+        pass
+
+    def resetClock(self):
+        pass
+
+    def skipClock(self):
+        pass
 
 pomodoroTimer()
 
