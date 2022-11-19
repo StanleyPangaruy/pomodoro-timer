99dc4f7cf00e8f4b709b61747e5943a27a04df35
unknown <pangaruystanley@gmail.com>
2022-11-19 14:34:59 +0800

    Implemeted the each sections timers with that start button working

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class pomodoroTimer:
         self.pomodoroTimerLabel = ttk.Label(self.tab1, text="25:00", font=("Ubuntu", 48))
         self.pomodoroTimerLabel.pack(pady=20)
 
-        self.shortBreakTimerLabel = ttk.Label(self.tab2, text="5:00", font=("Ubuntu", 48))
+        self.shortBreakTimerLabel = ttk.Label(self.tab2, text="05:00", font=("Ubuntu", 48))
         self.shortBreakTimerLabel.pack(pady=20)
 
         self.longBreakTimerLabel = ttk.Label(self.tab3, text="15:00", font=("Ubuntu", 48))
@@ -47,15 +47,65 @@ class pomodoroTimer:
         self.resetButton.grid(row=0, column=2)
 
         self.pomodoroCounterLabel = ttk.Label(self.gridLayout, text="Pomodoros: 0", font=("Ubuntu", 16))
-        self.pomodoroCounterLabel.grid(row=1, column=0, columnspan=3)
+        self.pomodoroCounterLabel.grid(row=1, column=0, columnspan=3, pady=10)
+
+        self.pomodoros = 0
+        self.skipped = False
+        self.stopped = False
 
         self.root.mainloop()
 
     def startTimerThread(self):
-        pass
+        t = threading.Thread(target=self.startTimer)
+        t.start()
 
     def startTimer(self):
-        pass
+        self.stopped = False
+        self.skipped = False
+        timerId = self.tabs.index(self.tabs.select()) + 1
+
+        if timerId == 1:
+            fullSecond = 60*25
+            while fullSecond > 0 and not self.stopped:
+                minutes, seconds =  divmod(fullSecond, 60)
+                self.pomodoroTimerLabel.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                fullSecond -= 1
+            if not self.stopped or self.skipped:
+                self.pomodoros += 1
+                self.pomodoroCounterLabel.config(text=f"Pomodoros: {self.pomodoros}")
+                if self.pomodoros % 4 == 0:
+                    self.tabs.select(2)
+                else:
+                    self.tabs.select(1)
+                self.startTimer()
+
+        elif timerId == 2:
+            fullSecond = 60*5
+            while fullSecond > 0 and not self.stopped:
+                minutes, seconds =  divmod(fullSecond, 60)
+                self.shortBreakTimerLabel.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                fullSecond -= 1
+            if not self.stopped or self.skipped:
+                self.tabs.select(0)
+                self.startTimer()
+
+        elif timerId == 3:
+            fullSecond = 60*15
+            while fullSecond > 0 and not self.stopped:
+                minutes, seconds =  divmod(fullSecond, 60)
+                self.longBreakTimerLabel.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                fullSecond -= 1
+            if not self.stopped or self.skipped:
+                self.tabs.select(0)
+                self.startTimer()
+        else:
+            print("Invalid timer id")
 
     def resetClock(self):
         pass
